3c38a66ac537fc609f1df7b22fca99d7e3581d63
MPSeven <chrisp3@daum.net>
2021-04-13 16:35:23 +0900

    bottom navigation menu

--- app/src/main/java/kr/go/mapo/intime/MainActivity.kt
+++ app/src/main/java/kr/go/mapo/intime/MainActivity.kt
@@ -1,11 +1,19 @@
 package kr.go.mapo.intime
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.messages.MessageListener
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
