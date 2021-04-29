faec620acb6e8494584da6473679238946c4b09e
CEmmanuelP3 <chrisp3@daum.net>
2021-04-29 13:38:05 +0900

    naver map fragment -> naver mapView

--- app/src/main/java/kr/go/mapo/intime/MainActivity.kt
+++ app/src/main/java/kr/go/mapo/intime/MainActivity.kt
@@ -33,29 +33,16 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var locationSource: FusedLocationSource
-    private lateinit var naverMap: NaverMap
-    private var markerList: MutableList<Marker> = mutableListOf()
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        replaceFragment(MapFragment())
+
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
         bottomNavigationView.menu.getItem(1).isChecked = true
-
-        val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map_fragment) as com.naver.maps.map.MapFragment?
-            ?: com.naver.maps.map.MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map_fragment, it).commit()
-            }
-
-        mapFragment.getMapAsync(this)
-
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
     private val onNavigationItemSelectedListener =
@@ -76,99 +63,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
             false
         }
-
-
     private val bottomNavigationView: BottomNavigationView by lazy {
         findViewById(R.id.bottomNavigationView)
     }
 
     private fun replaceFragment(fragment: Fragment) {
-
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frameLayout, fragment)
         fragmentTransaction.commit()
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated) {
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-            }
-            return
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
-        val locationOverlay = naverMap.locationOverlay
-        locationOverlay.isVisible = true
-
-        val uiSettings: UiSettings = naverMap.uiSettings
-        uiSettings.isLocationButtonEnabled = true
-        uiSettings.isCompassEnabled = false
-        uiSettings.isScrollGesturesEnabled = true
-
-        naverMap.addOnLocationChangeListener { location ->
-            Log.d(TAG, "locationLat: ${location.latitude}, locationLon: ${location.longitude}")
-            fetchAedLocation(
-                locationOverlay.position.latitude,
-                locationOverlay.position.longitude,
-                0.5F
-            )
-        }
-    }
-
-    private fun fetchAedLocation(lat: Double, lon: Double, km: Float) {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val aedService = retrofit.create(AedService::class.java)
-
-        aedService.getAedsByDistance(lat, lon, km)
-            .enqueue(object : Callback<AedDto> {
-                override fun onResponse(
-                    call: Call<AedDto>,
-                    response: Response<AedDto>
-                ) {
-                    if (response.isSuccessful.not()) {
-                        Log.e(TAG, "NOT SUCCESS!")
-                        return
-                    }
-
-                    response.body()?.let {
-                        it.aeds.forEach { sortedAed ->
-                            val marker = Marker()
-                            marker.position = LatLng(sortedAed.aed.lat, sortedAed.aed.lon)
-                            marker.map = naverMap
-
-                            markerList.add(marker)
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<AedDto>, t: Throwable) {
-                    Log.e(TAG, t.toString())
-                }
-
-            })
-
-    }
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
-        private const val TAG = "MainActivity"
-        private const val BASE_URL = "http://172.30.1.53:8080"
-    }
-
 
 }
--- app/src/main/java/kr/go/mapo/intime/fragment/MapFragment.kt
+++ app/src/main/java/kr/go/mapo/intime/fragment/MapFragment.kt
@@ -1,41 +1,36 @@
 package kr.go.mapo.intime.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import kr.go.mapo.intime.MainActivity
 import kr.go.mapo.intime.R
+import kr.go.mapo.intime.api.AedService
+import kr.go.mapo.intime.model.AedDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.security.Permissions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapFragment : Fragment(){
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mapView: MapView
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+    private var markerList: MutableList<Marker> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
     }
 
     override fun onCreateView(
@@ -43,28 +38,120 @@ class MapFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_map, container, false)
+        var mapView = rootView.findViewById(R.id.map_view) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        val locationOverlay = naverMap.locationOverlay
+        locationOverlay.isVisible = true
+
+        val uiSettings: UiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+        uiSettings.isCompassEnabled = false
+        uiSettings.isScrollGesturesEnabled = true
+
+        naverMap.addOnLocationChangeListener { location ->
+            Log.d(TAG, "locationLat: ${location.latitude}, locationLon: ${location.longitude}")
+            fetchAedLocation(
+                locationOverlay.position.latitude,
+                locationOverlay.position.longitude,
+                0.5F
+            )
+        }
+    }
+
+    private fun fetchAedLocation(lat: Double, lon: Double, km: Float) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val aedService = retrofit.create(AedService::class.java)
+
+        aedService.getAedsByDistance(lat, lon, km)
+            .enqueue(object : Callback<AedDto> {
+                override fun onResponse(
+                    call: Call<AedDto>,
+                    response: Response<AedDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT SUCCESS!")
+                        return
+                    }
+
+                    response.body()?.let {
+                        it.aeds.forEach { sortedAed ->
+                            val marker = Marker()
+                            marker.position = LatLng(sortedAed.aed.lat, sortedAed.aed.lon)
+                            marker.map = naverMap
+
+                            markerList.add(marker)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<AedDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                }
+            })
     }
 
     companion object {
-
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance() =
-            MapFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-            }
+        private const val TAG = "MainActivity"
+        private const val BASE_URL = "http://172.30.1.53:8080"
     }
 }
