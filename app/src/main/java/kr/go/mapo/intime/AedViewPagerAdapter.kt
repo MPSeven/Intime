f62a6a77d91a102d938f0c78f57d5b41cfd42c50
eugene <chrisp3@daum.net>
2021-05-14 15:25:08 +0900

    restore

--- app/src/main/java/kr/go/mapo/intime/AedViewPagerAdapter.kt
+++ app/src/main/java/kr/go/mapo/intime/AedViewPagerAdapter.kt
@@ -1,35 +1,34 @@
 package kr.go.mapo.intime
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.Button
+import android.widget.ListAdapter
 import android.widget.TextView
-import androidx.core.os.persistableBundleOf
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.CameraUpdate
+import kr.go.mapo.intime.fragment.MapFragment
 import kr.go.mapo.intime.model.SortedAed
-import org.w3c.dom.Text
 import java.lang.Exception
 import kotlin.math.roundToInt
 
-class AedListAdapter : ListAdapter<SortedAed, AedListAdapter.ItemViewHolder>(differ) {
+class AedViewPagerAdapter :
+    androidx.recyclerview.widget.ListAdapter<SortedAed, AedViewPagerAdapter.ItemViewHolder>(differ) {
 
-    inner class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    inner class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(sortedAed: SortedAed) {
-            val orgTextView = view.findViewById<TextView>(R.id.org)
-            val addressTextView = view.findViewById<TextView>(R.id.address)
-            val addressDetailTextView = view.findViewById<TextView>(R.id.addressDetail)
-            val distanceTextView = view.findViewById<TextView>(R.id.distance)
-            val findPathButton = view.findViewById<Button>(R.id.recyclerViewFindPathButton)
+            val orgTextView = view.findViewById<TextView>(R.id.orgMarker)
+            val addressTextView = view.findViewById<TextView>(R.id.addressMarker)
+            val addressDetailTextView = view.findViewById<TextView>(R.id.addressDetailMarker)
+            val distanceTextView = view.findViewById<TextView>(R.id.distanceMarker)
+            val findPathButton = view.findViewById<Button>(R.id.findPathButton)
 
             orgTextView.text = sortedAed.aed.org
             addressTextView.text = sortedAed.aed.address
@@ -37,7 +36,7 @@ class AedListAdapter : ListAdapter<SortedAed, AedListAdapter.ItemViewHolder>(dif
             distanceTextView.text = "${(sortedAed.distance * 1000).roundToInt()}m"
 
             view.setOnClickListener {
-                Log.d("View setOnClickListener", "${getItem(adapterPosition)}")
+                Log.d("ViewP2 onClickListener", "${getItem(adapterPosition)}")
             }
 
             findPathButton.setOnClickListener {
@@ -48,16 +47,21 @@ class AedListAdapter : ListAdapter<SortedAed, AedListAdapter.ItemViewHolder>(dif
                     }
                     it.context.startActivity(intent)
                 }catch (e: Exception) {
-                    val intentPlayStore = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${NAVER_MAP_PACKAGE_NAME}" ))
-                    it.context.startActivity(intentPlayStore)
+                   val intentPlayStore = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$NAVER_MAP_PACKAGE_NAME" ))
+                   it.context.startActivity(intentPlayStore)
                 }
             }
+
         }
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ItemViewHolder(inflater.inflate(R.layout.item_aed, parent, false))
+        return ItemViewHolder(inflater.inflate(R.layout.item_detail_viewpager, parent, false))
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
