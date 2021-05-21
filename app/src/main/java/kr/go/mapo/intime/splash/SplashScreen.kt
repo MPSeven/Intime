26233b4f729e38443a3558a21597fd8ce88307e6
yforyuri <yforyurirho@gmail.com>
2021-05-21 15:03:55 +0900

    fixed error

--- app/src/main/java/kr/go/mapo/intime/splash/SplashScreen.kt
+++ app/src/main/java/kr/go/mapo/intime/splash/SplashScreen.kt
@@ -1,27 +1,23 @@
-package kr.go.mapo.intime
+package kr.go.mapo.intime.splash
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kr.go.mapo.intime.R
 
-
-class SplashActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        @Suppress("DEPRECATION")
+/*        @Suppress("DEPRECATION")
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
         Handler().postDelayed({
             val intent = Intent(this,MainActivity::class.java )
             startActivity(intent)
             finish()
         },3000)
+    */
     }
 }
