bbb92fb555d526446e9b8836b220f1def610bea8
mornin9d3w <dlayoonji@gmail.com>
2021-05-25 18:04:30 +0900

    changes to onboarding and minor xml

--- app/src/main/java/kr/go/mapo/intime/info/AedActivity.kt
+++ app/src/main/java/kr/go/mapo/intime/info/AedActivity.kt
@@ -1,8 +1,23 @@
 package kr.go.mapo.intime.info
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_aed.*
 import kr.go.mapo.intime.R
 import kr.go.mapo.intime.databinding.ActivityAedBinding
 
@@ -14,9 +29,39 @@ class AedActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(findViewById(R.id.aed_appbar))
-        supportActionBar?.title = "재난상황 대처 방법"
+        supportActionBar?.title = "자동심장충격기 사용법"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        Log.i("SPAN", "calling clickableSpan")
+        clickableSpanOne()
 
+
+    }
+
+    private fun clickableSpanOne() {
+        val spanText = SpannableStringBuilder("심폐소생술 방법 바로 알아보기")
+        Log.i("SPAN", "spanText called")
+
+        val clickable = object : ClickableSpan() {
+            override fun updateDrawState(textPaint: TextPaint) {
+                textPaint.setARGB(1, 24, 116, 193)
+            }
+
+            override fun onClick(view: View) {
+                Log.i("SPAN", "intent called?")
+                startActivity(Intent(this@AedActivity, CprActivity::class.java))
+                view.invalidate()
+            }
+        }
+        spanText.setSpan(clickable, 0, spanText.length, 0)
+        spanText.setSpan(
+            ForegroundColorSpan(Color.parseColor("#1874C1")),
+            0,
+            spanText.length,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        val clickableId = findViewById<TextView>(R.id.info_tv_cpr_clickable)
+        clickableId.setText(spanText, TextView.BufferType.SPANNABLE)
+        clickableId.movementMethod = LinkMovementMethod.getInstance()
 
     }
 }
--- app/src/main/java/kr/go/mapo/intime/onboarding/OnBoardingOne.kt
+++ app/src/main/java/kr/go/mapo/intime/onboarding/OnBoardingOne.kt
@@ -1,35 +1,60 @@
 package kr.go.mapo.intime.onboarding
 
+import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_on_boarding_one.*
 import kotlinx.android.synthetic.main.fragment_on_boarding_one.view.*
 import kr.go.mapo.intime.R
+import kr.go.mapo.intime.databinding.FragmentOnBoardingOneBinding
 
 class OnBoardingOne : Fragment() {
+    private lateinit var spannableString: SpannableString
+    private var _binding: FragmentOnBoardingOneBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_on_boarding_one, container, false)
+        _binding = FragmentOnBoardingOneBinding.inflate(inflater, container, false)
+
         activity?.findViewById<ViewPager2>(R.id.vp)
-        view.findViewById<Button>(R.id.onBoarding_tb_skip).setOnClickListener {
+//        view.findViewById<Button>(R.id.onBoarding_tb_skip).setOnClickListener {
+//            findNavController().navigate(R.id.action_viewPagerFragment_to_mainActivity)
+//        }
+
+
+
+        binding.onBoardingTbSkip.setOnClickListener {
             findNavController().navigate(R.id.action_viewPagerFragment_to_mainActivity)
         }
-        return view
+
+        textOneSpan()
+        return binding.root
+
+    }
+
+    private fun textOneSpan(){
+        spannableString = SpannableString("일상 속 예상치 못한 응급상황!\n미리 대비하세요!")
+        val tfBold = StyleSpan(Typeface.BOLD)
+        spannableString.setSpan(tfBold, 12, 23, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        binding.onBoardingTvSlideHeader.text = spannableString
     }
 
 }
