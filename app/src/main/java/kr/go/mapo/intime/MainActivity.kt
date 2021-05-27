acd9174a45f0193fc8f586aecc72b830198f032f
mornin9d3w <dlayoonji@gmail.com>
2021-05-27 14:59:27 +0900

    app bar fixes?

--- app/src/main/java/kr/go/mapo/intime/MainActivity.kt
+++ app/src/main/java/kr/go/mapo/intime/MainActivity.kt
@@ -5,34 +5,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.naver.maps.map.*
-import kr.go.mapo.intime.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import kr.go.mapo.intime.info.InfoFragment
 import kr.go.mapo.intime.map.MapFragment
 import kr.go.mapo.intime.setting.SettingActivity
 import kr.go.mapo.intime.sos.SosFragment
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     private val infoFragment = InfoFragment()
     private val mapFragment = MapFragment()
     private val sosFragment = SosFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+//        frag manager 만들어서 이부분 다 바꾸기
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
         bottomNavigationView.menu.getItem(1).isChecked = true
         replaceFragment(mapFragment)
-
-        binding.toolbar.title = "INTIME"
+        toolbar.setTitle("INTIME")
+        toolbar.setTitleTextAppearance(this, R.style.Head3)
     }
 
 
@@ -44,7 +45,7 @@ class MainActivity : AppCompatActivity(){
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.toolbar_btn -> {
                 val settingIntent = Intent(this, SettingActivity::class.java)
                 startActivity(settingIntent)
@@ -53,36 +54,39 @@ class MainActivity : AppCompatActivity(){
         return super.onOptionsItemSelected(item)
     }
 
-//    네비바 컨트롤
-    private fun fragmentManager(){
+    //    네비바 컨트롤
+    private fun fragmentManager() {
         val fragManager = supportFragmentManager
         fragManager.beginTransaction().apply {
         }
     }
 
-//    바텀네비바
+    //    바텀네비바
     private val onNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_sos -> {
 
                     replaceFragment(SosFragment())
-                    binding.toolbar.title = ""
-                    binding.toolbarMain.text ="SOS"
+                    toolbar.setTitle("SOS")
+                    toolbar.setTitleCentered(true)
+                    toolbar.setTitleTextAppearance(this, R.style.Body1 )
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.menu_map -> {
 
                     replaceFragment(MapFragment())
-                    binding.toolbarMain.text = ""
+                    toolbar.setTitle("INTIME")
+                    toolbar.setTitleCentered(false)
+                    toolbar.setTitleTextAppearance(this, R.style.Head3)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.menu_info -> {
 
                     replaceFragment(InfoFragment())
-                    binding.toolbar.title = ""
-                    binding.toolbarMain.text ="정보"
-
+                    toolbar.setTitle("정보")
+                    toolbar.setTitleCentered(true)
+                    toolbar.setTitleTextAppearance(this, R.style.Body1 )
                     return@OnNavigationItemSelectedListener true
                 }
             }
